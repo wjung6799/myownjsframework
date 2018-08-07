3299983a251ca82335e8a3aa2c5353d1abb78839
Won Jung <won.jung@pacific.costcotravel.com>
2018-08-07 10:14:34 -0700

    domSelector

--- wonframework.js
+++ wonframework.js
@@ -1,4 +1,22 @@
-function domObjectify(dom) {
-    var domObject = document.querySelector(dom);
-    return domObject;
+function DomObjectify(dom, idName, classList) {
+    var selectedDom = document.querySelector(dom);
+	this.dom = function() {
+		return selectedDom;
+	}
+	this.idName = function() {
+		return selectedDom.id;
+	}
+	this.classList = function() {
+		return selectedDom.classList;
+	}
 }
+
+function test() {
+	console.log("test is running");
+	var p = new DomObjectify("p");
+	console.log(p.dom());
+	console.log(p.idName());
+	console.log(p.classList());
+}
+
+test();
